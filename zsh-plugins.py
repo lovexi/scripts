00ee320cb44e0d8bd73161fd51b07b9fd9cfaae8
lovexi <scanletfever@gmail.com>
2016-08-06 11:57:49 -0700

    Add list feature to list all avaiable zsh plugins

--- zsh-plugins.py
+++ zsh-plugins.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import sys, getopt
+from os import listdir
 from os.path import isfile, expanduser
 from file.replace import replace
 
@@ -13,6 +14,7 @@ optional arguments:
 -h, --help                show help message for user
 -a, --add plugin_name	  add plugin in zsh conf file
 -r, --remove plugin_name  remove plugin in zsh conf file
+-l, --list                list all plugins in oh-my-zsh plugins directory
 """
 operation = ''
 
@@ -26,13 +28,40 @@ success_message = 'Successfully {!s} plugin in zshrc config file.\nPlease source
 def usage(complete_type = 0):
 	if complete_type == 1:
 		print ('Error: Wrong usage for this command.')
-	print (help_message)
+	print(help_message)
+	sys.exit()
+
+def list_plugin(zsh_plugin_path):
+	print(listdir(zsh_plugin_path))
 	sys.exit()
 
 def check_file(file_path, file_name):
 	if not isfile(file_path + file_name):
-		print (not_exist_message.format(file_name))
+		print(not_exist_message.format(file_name))
 		sys.exit()
+
+def add_plugin(zshrc_file, plugin_name):
+	for line in zshrc_file:
+		if line.startswith('plugins'):
+			if plugin_name in line:
+				print (duplicate_message.format(plugin_name))
+				sys.exit()
+				line_length = len(line)
+				new_line = line[: line_length - 2] + '{!s} '.format(plugin_name) + line[line_length - 2 :]
+				replace(zshrc_path, line, new_line)
+
+def remove_plugin(zshrc_file, plugin_name):
+	for line in zshrc_file:
+		if line.startswith('plugins'):
+			if plugin_name not in line:
+				print (not_exist_message.format(plugin_name))
+				sys.exit()
+				plugin_index = line.find(plugin_name)
+				if not line[plugin_index - 1].isspace() or not line[plugin_index + len(plugin_name)].isspace():
+					print (not_exist_message.format(plugin_name))
+					sys.exit()
+				new_line = line[: plugin_index] + line[plugin_index + len(plugin_name) + 1 :]
+				replace(zshrc_path, line, new_line)
 
 def main(argv):
 	# Find path to .vimrc file
@@ -43,7 +72,7 @@ def main(argv):
 
 	# Parsing args
 	try:
-		opts, args = getopt.getopt(argv, "ha:r:", ["help", "add", "remove"])
+		opts, args = getopt.getopt(argv, "ha:r:l", ["help", "add", "remove", "list"])
 	except getopt.GetoptError:
 		usage(1)
 	if len(opts) == 0:
@@ -51,35 +80,22 @@ def main(argv):
 	for opt, arg in opts:
 		if opt in ('-h', '--help'):
 			usage()
+
+		elif opt in ('-l', '--list'):
+			list_plugin(zsh_plugin_path)
+
 		elif opt in ('-a', '--add'):
 			operation = 'add'
 			plugin_name = arg
 			print (adding_message.format(plugin_name))
-
 			check_file(zsh_plugin_path, plugin_name)
-			for line in zshrc_file:
-				if line.startswith('plugins'):
-					if plugin_name in line:
-						print (duplicate_message.format(plugin_name))
-						sys.exit()
-					line_length = len(line)
-					new_line = line[: line_length - 2] + '{!s} '.format(plugin_name) + line[line_length - 2 :]
-					replace(zshrc_path, line, new_line)
+			add_plugin(zshrc_file, plugin_name)
+
 		elif opt in ('-r', '--remove'):
 			operation = 'remove'
 			plugin_name = arg
 			print (removing_message.format(plugin_name))
-			for line in zshrc_file:
-				if line.startswith('plugins'):
-					if plugin_name not in line:
-						print (not_exist_message.format(plugin_name))
-						sys.exit()
-					plugin_index = line.find(plugin_name)
-					if not line[plugin_index - 1].isspace() or not line[plugin_index + len(plugin_name)].isspace():
-						print (not_exist_message.format(plugin_name))
-						sys.exit()
-					new_line = line[: plugin_index] + line[plugin_index + len(plugin_name) + 1 :]
-					replace(zshrc_path, line, new_line)
+			remove_plugin(zshrc_file, plugin_name)
 
 	print (success_message.format(operation))
 
